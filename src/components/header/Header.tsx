3a293fdfd5dc99c0f42fef28314af121eb028233
Jakub Žitník <klindkubak@zohomail.eu>
2023-03-14 17:38:28 +0100

    Přidáno menu pro mobilní verzi.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,34 @@
 import "./header.css"
+import { useState } from "react";
 import MenuItems from "../../config/menu"
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
 function Header() {
+    const [ display, setdisplay ] = useState('none');
+    function toggleMenu() {
+        if (display == "none") {
+            setdisplay("flex")
+        }
+        else {
+            setdisplay("none")
+        }
+    }
     const menu = MenuItems.map((name) => {
         return <NavLink className="el" to={name[1]}><span>{name[0]}</span></NavLink>
+    })
+    const menuMobile = MenuItems.map((name) => {
+        return <Link onClick={toggleMenu} to={name[1]}><span>{name[0]}</span></Link>
     })
     return (
         <header>
             <span className="title"><NavLink to="/">KLIND - OS</NavLink></span>
             <div className="menu">{menu}</div>
+            <div className="menu-btn" onClick={toggleMenu}></div>
+            <div className="menu-mobile" style={{'display': display}}>
+                <div className="menu-btn-mobile" onClick={toggleMenu}></div>
+                <Link onClick={toggleMenu} to={"/"}><span>KLIND - OS</span></Link>
+                {menuMobile}
+            </div>
         </header>
     )
 }
