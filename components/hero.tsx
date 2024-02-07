3b81ef6ed4fe1e35fcdee55c01b55a6f19007403
Jakub Žitník <email@jzitnik.is-a.dev>
2024-02-07 08:37:32 +0100

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -32,7 +32,7 @@ export default function Hero() {
                 <Link className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="/download">Stáhnout</Link>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://klind-os-docs.onrender.com/" target='_blank'>Dokumentace</a>
+                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://docs.klindos.jzitnik.dev/" target='_blank'>Dokumentace</a>
               </div>
             </div>
           </div>
